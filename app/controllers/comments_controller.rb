470c8d651aabee54c167846c454d6605c20ef16b
Cory Hack <cory.hack@gmail.com>
2018-02-03 15:34:19 +0000

    Add Carousel slider

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,26 +7,9 @@ class CommentsController < ApplicationController
     redirect_to place_path(@place)
   end 
 
-
-
-  # def create
-  #     @place = Place.find(params[:place_id])
-  #     if @place.comments.create(comment_params).valid?
-  #       @place.comments.create(comment_params.merge(user: current_user))   
-  #       redirect_to place_path(@place)
-  #     else
-  #      # render :show, status: :unprocessable_entity
-  #       return render text: 'Error: Comment message was not included.', status: :forbidden
-  #     end
-  # end
-
-
-
-
-
   private
 
   def comment_params
-    params.require(:comment).permit(:message, :rating)
+    params.require(:comment).permit(:message)
   end
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,26 +1,12 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
 
-  #def create
-   # @place = Place.find(params[:place_id])
-    #@place.photos.create(photo_params)
-    #redirect_to place_path(@place)
-  #end
-
   def create
-      @place = Place.find(params[:place_id])
-      if @place.photos.create(photo_params).valid?
-        @place.photos.create(photo_params.merge(user: current_user))   
-        redirect_to place_path(@place)
-      else
-       # render :show, status: :unprocessable_entity
-        return render text: 'Error: Picture was not included or failed to load.', status: :forbidden
-      end
+   @place = Place.find(params[:place_id])
+    @place.photos.create(photo_params)
+    redirect_to place_path(@place)
   end
-
-
-
-
+  
   private
 
   def photo_params
